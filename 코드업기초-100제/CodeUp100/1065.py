658077ddc7ec647c53ff2b0bf4cfc816c8924677
taewon <kimtae9217@gmail.com>
2021-02-15 21:48:59 +0900

    오늘도 한건!

---
+++ 코드업기초-100제/CodeUp100/1065.py
@@ -0,0 +1,15 @@
+a, b, c = map(int, input().split())
+if(a % 2  == 0):
+    print(a)
+if(b % 2 == 0):
+    print(b)
+if(c % 2 == 0):
+    print(c)
+
+# For문을 이용한 풀이
+# a = map(int, input().split())
+# for i in a:
+#     if(i % 2  == 0):
+#         print(i)
+
+
